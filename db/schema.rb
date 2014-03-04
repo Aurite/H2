6f486cb3a5744dc314588700edb4d582b2613c6c
Mori <keitamori@gmail.com>
2014-03-04 18:31:32 +0100

    Ajout de la columne fin d'evenement

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20140304164037) do
+ActiveRecord::Schema.define(version: 20140304172700) do
 
   create_table "events", force: true do |t|
     t.string   "name"
@@ -20,6 +20,7 @@ ActiveRecord::Schema.define(version: 20140304164037) do
     t.text     "description_event"
     t.datetime "created_at"
     t.datetime "updated_at"
+    t.datetime "end_of_event"
   end
 
   create_table "roles", force: true do |t|
