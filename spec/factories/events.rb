47639e3bb0f4bd900b82ea57ef7a06cbec518c8c
Mori <keitamori@gmail.com>
2014-03-05 12:01:58 +0100

    annotation des tables par annotate

--- spec/factories/events.rb
+++ spec/factories/events.rb
@@ -1,3 +1,17 @@
+# == Schema Information
+#
+# Table name: events
+#
+#  id                     :integer          not null, primary key
+#  name                   :string(255)
+#  location               :string(255)
+#  date_and_time_of_event :datetime
+#  description_event      :text
+#  created_at             :datetime
+#  updated_at             :datetime
+#  end_of_event           :datetime
+#
+
 # Read about factories at https://github.com/thoughtbot/factory_girl
 
 FactoryGirl.define do
