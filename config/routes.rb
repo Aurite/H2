fb18ec224ae16d3051e9e3a919bfccac1befcf8e
Mori <keitamori@gmail.com>
2014-03-05 16:31:24 +0100

    ajout d'une route pour les evenement et leur participation

--- config/routes.rb
+++ config/routes.rb
@@ -1,5 +1,6 @@
 H2::Application.routes.draw do
   resources :events
+  get '/event/participate' =>'events#participate'
 
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
