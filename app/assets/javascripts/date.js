74a3dec608d0a1cb381acbe2d1054fc286676328
Mori <keitamori@gmail.com>
2014-03-06 17:12:08 +0100

    programmae js de drag and drop plus boite de dialogue hacker helper

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,12 +1,25 @@
-  $(function() {
+/*Programme gestion drag and drop */  
+$(function() {
     $( "#draggable" ).draggable();
     $( "#droppable" ).droppable({
       drop: function( event, ui ) {
         $( this )
           .addClass( "ui-state-highlight" )
           .find( "p" )
-            .html( "Dropped!" );
-            confirm("Souhaitez vous participer a l'evenement ?");
+            .html( "Dropped!" )
+            .dialog({
+                resizable: false,
+                height:    140,
+                modal:     true,
+                buttons:{
+                    "Hacker": function(){
+                            $( this ).dialog( "close");
+                    },
+                    "Helper": function() {
+                            $( this ).dialog( "close" );
+                    }
+            }
+       });
       }
     });
   });
