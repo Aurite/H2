15277e8d98ad9c68e63efff5d983ff9a36c56b9a
Mori <keitamori@gmail.com>
2014-03-06 14:24:40 +0100

    ajout de route personnalise connection square interface

--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,7 @@
 H2::Application.routes.draw do
   resources :events
   get '/event/participate' =>'events#participate'
-
+  get '/event/interface' => 'events#interface'
   root :to => "home#index"
   devise_for :users, :controllers => {:registrations => "registrations"}
   resources :users
