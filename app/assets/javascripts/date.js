6e856324d406a770a3e4b421460057c8adffa930
Mori <keitamori@gmail.com>
2014-03-07 18:08:13 +0100

    modification du jquery pour une nouvelle vue plus cool

--- app/assets/javascripts/date.js
+++ app/assets/javascripts/date.js
@@ -1,35 +1,51 @@
 /*Programme gestion drag and drop */  
 
-var hacker_color=function(){
-    $(".message_inscription").html("Vous etes inscrit en tant que participant");
-}
+// var hacker_color=function(){
+//     $(".message_inscription").html("Vous etes inscrit en tant que participant");
+// };
 
-var helper_color=function(){
-    $(".message_inscription").html("Vous etes inscrit en tant que helper");
-}
+// var helper_color=function(){
+//     $(".message_inscription").html("Vous etes inscrit en tant que helper");
+// };
 
 
-$(function() {
-    $( "#draggable" ).draggable();
-    $( "#droppable" ).droppable({
-      drop: function( event, ui ) {
-        $( this )
-          .addClass( "ui-state-highlight" )
-          .find( "p" )
-            .html( "Wanna join as " )
-            .dialog({
-                resizable: false,
-                height:    140,
-                modal:     true,
-                buttons:{
-                    "Hacker": function(){
-                            $( this ).dialog( hacker_color());
-                    },
-                    "Helper": function() {
-                            $( this ).dialog( helper_color());
-                    }
-            }
-       });
-      }
-    });
-  });
+// $(function() {
+//     $( "#draggable" ).draggable();
+//     $( "#droppable" ).droppable({
+//       drop: function( event, ui ) {
+//         $( this )
+//           .addClass( "ui-state-highlight" )
+//           .find( "p" )
+//             .html( "Wanna join as " )
+//             .dialog({
+//                 resizable: false,
+//                 height:    140,
+//                 modal:     true,
+//                 buttons:{
+//                     "Hacker": function(){
+//                             $( this ).dialog( hacker_color());
+//                     },
+//                     "Helper": function() {
+//                             $( this ).dialog( helper_color());
+//                     }
+//             }
+//        });
+//       }
+//     });
+//   });
+
+    // body...
+    $("a.btn.btn-sm.btn-info").click(function(){
+
+        alert("bouton a click");
+    });
+    
+    $("a.btn.btn-sm.btn-primary").click(function(){
+
+        alert("bouton b click");
+    });
+
+
+    
+
+
